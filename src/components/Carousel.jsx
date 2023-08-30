3b4ae9b3752919ea02acf4e953050b4db2a2f1c8
stormymayday <stormymayday7@gmail.com>
2023-08-29 19:02:08 -0700

    Implemented Carousel.jsx component

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import { shortList, list, longList } from '../data.js';
+import { FaQuoteRight } from 'react-icons/fa';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+const Carousel = () => {
+
+    const [people, setPeople] = useState(shortList);
+
+    const prevSlide = () => {
+
+    };
+
+    const nextSlide = () => {
+
+    };
+
+    return (
+        <section className='slider-container'>
+            {
+                people.map((person) => {
+
+                    const { id, image, name, title, quote } = person;
+
+                    return (
+                        <article key={id} className='slide'>
+                            <img className='person-img' src={image} alt={name} />
+                            <h5 className='name'>{name}</h5>
+                            <p className="title">{title}</p>
+                            <p className="text">{quote}</p>
+                            <FaQuoteRight className='icon' />
+                        </article>
+                    )
+                })
+            }
+
+            <button type='button' className='prev' onClick={prevSlide}>
+                <FiChevronLeft />
+            </button>
+
+            <button type='button' className='next' onClick={nextSlide}>
+                <FiChevronRight />
+            </button>
+
+        </section>
+    );
+}
+
+export default Carousel;
